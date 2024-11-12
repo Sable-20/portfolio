d09b2e67e1795f0b513d1b1f84dccd7944c699a6
TheOkayDev <72171476+Sable-20@users.noreply.github.com>
2024-11-12 17:10:23 -0500

    Delete src directory

--- src/hooks.server.ts
+++
@@ -1,6 +0,0 @@
-import type { Handle } from '@sveltejs/kit';
-import { i18n } from '$lib/i18n';
-import { prepareStylesSSR } from '@svelteuidev/core';
-const handleParaglide: Handle = i18n.handle();
-import { sequence } from '@sveltejs/kit/hooks';
-export const handle = sequence(handleParaglide, prepareStylesSSR);
